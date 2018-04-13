b48b0dcadc6274b688dd5724c931bd77219f5274
Dennis Bilson <quabynahdennis@gmail.com>
2018-04-13 04:33:21 +0000

    Altered Firebase JS

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,31 @@
-const firebase = require('firebase');
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDyO2pWA3OonAXIsHrvKKkeWhVHpkI9Bvs",
+        authDomain: "phoenix-master.firebaseapp.com",
+        databaseURL: "https://phoenix-master.firebaseio.com",
+        projectId: "phoenix-master",
+        storageBucket: "phoenix-master.appspot.com",
+        messagingSenderId: "118933953659"
+    };
+    firebase.initializeApp(config);
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDyO2pWA3OonAXIsHrvKKkeWhVHpkI9Bvs",
-    authDomain: "phoenix-master.firebaseapp.com",
-    databaseURL: "https://phoenix-master.firebaseio.com",
-    projectId: "phoenix-master",
-    storageBucket: "phoenix-master.appspot.com",
-    messagingSenderId: "118933953659"
+    console.log(firebase.app());
 
-};
-firebase.initializeApp(config);
+    firebase.firestore().enablePersistence()
+        .then(function() {
+            // Initialize Cloud Firestore through firebase
+            var db = firebase.firestore();
+        })
+        .catch(function(err) {
+            if (err.code == 'failed-precondition') {
+                // Multiple tabs open, persistence can only be enabled
+                // in one tab at a a time.
+                // ...
+            } else if (err.code == 'unimplemented') {
+                // The current browser does not support all of the
+                // features required to enable persistence
+                // ...
+            }
+        });
+});
