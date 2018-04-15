161e4cf79fe402c4e9f8bfc73b89654e3489d9ea
Dennis Bilson <quabynahdennis@gmail.com>
2018-04-15 04:54:16 +0000

    Improved again

--- public/assets/js/inventory.js
+++ public/assets/js/inventory.js
@@ -59,23 +59,6 @@ var getUser = function(user){
       if (doc.exists) {
           console.log("Document data:", doc.data());
 
-          // Persist user login state
-          firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-            .then(function() {
-              // Existing and future Auth states are now persisted in the current
-              // session only. Closing the window would clear any existing state even
-              // if a user forgets to sign out.
-              // ...
-              // New sign-in will be persisted with session persistence.
-              return firebase.auth().signInWithEmailAndPassword(doc.data().email, doc.data().password);
-            })
-            .catch(function(error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              console.log(errorMessage);
-            });
-
       } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
